d4241a5b98f040096ed269afbc4310d53d70318f
Olin-h <1660266951@qq.com>
2020-03-20 21:57:26 +0800

    V_HLayout

--- src/ui/RunVertical_HorizontalLayout.py
+++ src/ui/RunVertical_HorizontalLayout.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = QMainWindow()
     ui = Vertical_HorizontalLayout.Ui_MainWindow()
+
     # 向主窗口添加控件
     ui.setupUi(mainWindow)
     mainWindow.show()
