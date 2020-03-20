5036ef5cc53f0e37598f51b0334993e3c65d5546
Olin-h <1660266951@qq.com>
2020-03-20 21:44:53 +0800

    V_HLayout

---
+++ src/ui/Vertical_HorizontalLayout.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Vertical_HorizontalLayout.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.layoutWidget.setGeometry(QtCore.QRect(60, 180, 641, 91))
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.layoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(self.layoutWidget)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.textEdit = QtWidgets.QTextEdit(self.layoutWidget)
+        self.textEdit.setObjectName("textEdit")
+        self.verticalLayout.addWidget(self.textEdit)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label_2 = QtWidgets.QLabel(self.layoutWidget)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout.addWidget(self.label_2)
+        self.textEdit_2 = QtWidgets.QTextEdit(self.layoutWidget)
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.horizontalLayout.addWidget(self.textEdit_2)
+        self.checkBox = QtWidgets.QCheckBox(self.layoutWidget)
+        self.checkBox.setObjectName("checkBox")
+        self.horizontalLayout.addWidget(self.checkBox)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.label_3 = QtWidgets.QLabel(self.layoutWidget)
+        self.label_3.setObjectName("label_3")
+        self.horizontalLayout_2.addWidget(self.label_3)
+        self.textEdit_3 = QtWidgets.QTextEdit(self.layoutWidget)
+        self.textEdit_3.setObjectName("textEdit_3")
+        self.horizontalLayout_2.addWidget(self.textEdit_3)
+        self.checkBox_2 = QtWidgets.QCheckBox(self.layoutWidget)
+        self.checkBox_2.setObjectName("checkBox_2")
+        self.horizontalLayout_2.addWidget(self.checkBox_2)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "备份"))
+        self.label_2.setText(_translate("MainWindow", "姓名："))
+        self.checkBox.setText(_translate("MainWindow", "CheckBox"))
+        self.label_3.setText(_translate("MainWindow", "职务："))
+        self.checkBox_2.setText(_translate("MainWindow", "CheckBox"))
